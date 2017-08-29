f33d4bf40b29cfb9e992c992a9345b1005098d02
liang hao <lhao@merkleinc.com>
2017-08-29 11:01:07 +0800

    add rewrite

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -17,64 +17,99 @@ class IndexController extends RestController {
                 break;
             case 'post': // post请求处理代码
                 $data=json_decode(file_get_contents("php://input"),true);
-                $this->response(D('interviewer')->addData($data),'json');
+                if($id==0)
+                    $this->response(D('interviewer')->addData($data),'json');
+                else
+                    $this->response(D('interviewer')->save($data),'json');
                 break;
             case 'delete': // get请求处理代码
                 $this->response(D('interviewer')->deleteData(array("interviewer_id"=>$id)),'json');
                 break;
         }
     }
-    public function interview($id=0,$type=0){
+    public function interview($id=-1,$type=-1){
         switch ($this->_method){
             case 'get': // get请求处理代码
-                $data=D('interview')->where(array('candidate_id'=>$id))->order('type')->select();
-                $res['candidateID']=$id;
-                $res['candidateName']=D('candidate')->field('name')->find($id)['name'];
-                $res['interviewerID']=[0,0,0,0,0,0];
-                $res['interviewerName']=["","","","","",""];
-                $res['comment']=["","","","","",""];
-                $res['weight']=["","","","","","","","",""];
-                $res['date']='';
-                $res['pos']='';
-                $res['score']=[["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""]];
-                $res['q']=[["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""]];
-                foreach ( $data as $record){
-                    $res['interviewerID'][$record[type]]=$record['interviewerID'];
-                    $res['interviewerName'][$record[type]]=D('interviewer')->field('name')->find($record['interviewerID'])['name'];
-                    $res['comment'][$record[type]]=$record['comment'];
-                    $res['score'][$record[type]][0]=$record[score1];
-                    $res['score'][$record[type]][1]=$record[score2];
-                    $res['score'][$record[type]][2]=$record[score3];
-                    $res['score'][$record[type]][3]=$record[score4];
-                    $res['score'][$record[type]][4]=$record[score5];
-                    $res['score'][$record[type]][5]=$record[score6];
-                    $res['score'][$record[type]][6]=$record[score7];
-                    $res['score'][$record[type]][7]=$record[score8];
-                    $res['score'][$record[type]][8]=$record[score9];
-                    $res['q'][$record[type]][0]=$record[q1];
-                    $res['q'][$record[type]][1]=$record[q2];
-                    $res['q'][$record[type]][2]=$record[q3];
-                    $res['q'][$record[type]][3]=$record[q4];
-                    $res['q'][$record[type]][4]=$record[q5];
-                    $res['q'][$record[type]][5]=$record[q6];
-                    $res['q'][$record[type]][6]=$record[q7];
-                    $res['q'][$record[type]][7]=$record[q8];
-                    $res['q'][$record[type]][8]=$record[q9];
-                }
-                $this->response(array($res),'json');
+                if($id==-1||$type==-1)
+                    $this->response(null,'json');
+                $all=M('vw_interview')->where(array('candidate_id'=>$id,'type'=>$type))->find();
+                if(empty($all))
+                    $this->response(array(0),'json');
+                $interview=M('vw_interview')->distinct(true)->field('interview_id,interviewer_id,interviewer_name,type,sum,passed,comment')->where(array('candidate_id'=>$id,'type'=>$type))->find();
+                $score=M('vw_interview')->distinct(true)->field('score_cd,score,question,weight')->where(array('candidate_id'=>$id,'type'=>$type))->select();
+                $common=M('vw_interview')->distinct(true)->field('score_cd,score,weight')->where(array('candidate_id'=>$id,'type'=>6))->select();
+                $this->response(array(array('interview'=>$interview,'score'=>$score,'common'=>$common)),'json');
                 break;
             case 'put': // put请求处理代码
-//                $data=json_decode(file_get_contents("php://input"),true);
-                //$map['interviewer_id'] = $id;
-//                $this->response(D('interviewer')->save($data),'json');
+                $data=json_decode(file_get_contents("php://input"),true);
+                $interview_id=$data['interview1']['interview_id'];
+                if(empty($interview_id))
+                    $this->response(array(0),'json');
+                D('interview')->field('interview_id,candidate_id,interviewer_id,type,comment,created_date,created_by')
+                    ->save(array('interview_id'=>$interview_id,'candidate_id'=>$data['candidate_id'],'interviewer_id'=>$interview1[interviewer_id],
+                        'type'=>$data['interview1'][type], 'comment'=>$data['interview1'][comment],'created_date'=>date("Y-m-d"),'created_by'=>$_SERVER['LOGON_USER']));
+                $i=$interview_id;
+                D('score')->where(array('interview_id'=>$i))->delete();
+                for($x=0;$x<count($data['interview1'][score]);$x++){
+                    if(!empty($data['interview1'][weight][$x])){
+                        M('score')->add(array('interview_id'=>$i,'score_cd'=>$x,'score'=>$data['interview1'][score][$x],
+                            'question'=>$data['interview1'][q][$x],'weight'=>$data['interview1'][weight][$x]));
+                    }
+                }
+                $j=D('interview')->where(array('candidate_id'=>$data['candidate_id'],'type'=>6))->find();
+                $j=$j['interview_id'];
+                if(empty($j)){
+                    $j=D('interview')->field('candidate_id,interviewer_id,type,created_date,created_by')
+                        ->addData(array('candidate_id'=>$data['candidate_id'],'interviewer_id'=>$data['interview1'][interviewer_id],'type'=>6,
+                            'created_date'=>date("Y-m-d"),'created_by'=>$_SERVER['LOGON_USER']));
+                }
+                D('score')->where(array('interview_id'=>$j))->delete();
+                for($x=0;$x<count($data['interview2'][score]);$x++){
+                    if(!empty($data['interview2'][weight][$x])) {
+                        M('score')->add(array('interview_id' => $j, 'score_cd' => $x, 'score' => $data['interview2'][score][$x],
+                            'question' => $data['interview2'][q][$x], 'weight' => $data['interview2'][weight][$x]));
+                    }
+                }
+                //call procedure to update status
+                M()->execute("call update_status('$data[candidate_id]')");
                 break;
             case 'post': // post请求处理代码
                 $data=json_decode(file_get_contents("php://input"),true);
-//                dump(D('interview')->field('sum')->where(array('candidate_id'=>$data['candidate_id'],'type'=>1))->find());
                 $data['created_date']=date("Y-m-d");
                 $data['created_by']=$_SERVER['LOGON_USER'];
+                if($id == -1)
+                    $i=D('interview')->field('candidate_id,interviewer_id,type,comment,created_date,created_by')
+                    ->addData(array('candidate_id'=>$data['candidate_id'],'interviewer_id'=>$data['interview1'][interviewer_id],'type'=>$data['interview1'][type],
+                        'comment'=>$data['interview1'][comment],'created_date'=>date("Y-m-d"),'created_by'=>$_SERVER['LOGON_USER']));
+                else{
+                    $i=$id;
+                    D('interview')->field('interview_id,candidate_id,interviewer_id,type,comment,created_date,created_by')
+                    ->save(array('interview_id'=>$id,'candidate_id'=>$data['candidate_id'],'interviewer_id'=>$data['interview1'][interviewer_id],
+                        'type'=>$data['interview1'][type], 'comment'=>$data['interview1'][comment],'created_date'=>date("Y-m-d"),'created_by'=>$_SERVER['LOGON_USER']));
+                }
+                D('score')->where(array('interview_id'=>$i))->delete();
+                for($x=0;$x<count($data['interview1'][score]);$x++){
+                    if(!empty($data['interview1'][weight][$x])) {
+                        M('score')->add(array('interview_id' => $i, 'score_cd' => $x, 'score' => $data['interview1'][score][$x],
+                            'question' => $data['interview1'][q][$x], 'weight' => $data['interview1'][weight][$x]));
+                    }
+                }
+                $j=D('interview')->where(array('candidate_id'=>$data['candidate_id'],'type'=>6))->find();
+                $j=$j['interview_id'];
+                if(empty($j)){
+                    $j=D('interview')->field('candidate_id,interviewer_id,type,created_date,created_by')
+                        ->addData(array('candidate_id'=>$data['candidate_id'],'interviewer_id'=>$data['interview1'][interviewer_id],'type'=>6,
+                            'created_date'=>date("Y-m-d"),'created_by'=>$_SERVER['LOGON_USER']));
+                }
+                D('score')->where(array('interview_id'=>$j))->delete();
+                for($x=0;$x<count($data['interview2'][score]);$x++){
+                    if(!empty($data['interview2'][weight][$x])) {
+                        M('score')->add(array('interview_id' => $j, 'score_cd' => $x, 'score' => $data['interview2'][score][$x],
+                            'question' => $data['interview2'][q][$x], 'weight' => $data['interview2'][weight][$x]));
+                    }
+                }
                 //calculate sum
-                if($data['type']==0){
+                /*if($data['type']==0){
                     $data['sum']=$data['score1']*0.4+$data['score2']*0.1+$data['score3']+$data['score4']*0.15+$data['score5']*0.05+
                         $data['score6']*0.2+$data['score7']+$data['score8']+$data['score9'];
                 }else{
@@ -84,13 +119,16 @@ class IndexController extends RestController {
                 $data['passed']=0;
                 D('interview')->deleteData(array("candidate_id"=>$data[candidate_id],"type"=>$data[type]));
                 //insert
-                $i=D('interview')->addData($data);
+                $i=D('interview')->addData($data);*/
                 //call procedure to update status
                 M()->execute("call update_status('$data[candidate_id]')");
                 $this->response($i,'json');
                 break;
             case 'delete': // get请求处理代码
 //                $this->response(D('interviewer')->deleteData(array("interviewer_id"=>$id)),'json');
+                break;
+            case 'options':
+                $this->response(0,json);
                 break;
         }
     }
@@ -134,6 +172,9 @@ class IndexController extends RestController {
     public function searchCandidate($name){
         switch ($this->_method){
             case 'get': // get请求处理代码
+                if(isset($_SERVER['HTTP_X_ORIGINAL_URL'])){
+                    $name=substr($_SERVER['HTTP_X_ORIGINAL_URL'],strrpos($_SERVER['HTTP_X_ORIGINAL_URL'],'/')); 
+                }
                 if(empty($name)){
                     //$this->response(D('candidate')->select(),'json');
                 }else{
@@ -147,7 +188,42 @@ class IndexController extends RestController {
                 break;
         }
     }
-
+    public function searchCandidateCN($name){
+        switch ($this->_method){
+            case 'get': // get请求处理代码
+                if(isset($_SERVER['HTTP_X_ORIGINAL_URL'])){
+                    $name=urldecode(substr($_SERVER['HTTP_X_ORIGINAL_URL'],strrpos($_SERVER['HTTP_X_ORIGINAL_URL'],'/')+1)); 
+                }
+                $this->response($name,'json');
+                if(empty($name)){
+                    //$this->response(D('candidate')->select(),'json');
+                }else{
+                    $map['name'] = array('like','%'.$name.'%');
+                    $res=D('candidate_desc_cn')->where($map)->order('candidate_id')->limit(10)->select();
+                    if(empty($res)){
+                        $res=array(array('candidate_id'=>'','name'=>'','description'=>''));
+                    }
+                    $this->response($res,'json');
+                }
+                break;
+        }
+    }
+    public function searchCandidateEN($name){
+        switch ($this->_method){
+            case 'get': // get请求处理代码
+                if(empty($name)){
+                    //$this->response(D('candidate')->select(),'json');
+                }else{
+                    $map['name'] = array('like','%'.$name.'%');
+                    $res=D('candidate_desc_en')->where($map)->order('candidate_id')->limit(10)->select();
+                    if(empty($res)){
+                        $res=array(array('candidate_id'=>'','name'=>'','description'=>''));
+                    }
+                    $this->response($res,'json');
+                }
+                break;
+        }
+    }
     public function searchInterviewer($name){
         switch ($this->_method){
             case 'get': // get请求处理代码
@@ -165,16 +241,16 @@ class IndexController extends RestController {
         }
     }
 
-    public function upload($name){
+    public function upload($name,$dept){
         $upload = new \Think\Upload();// 实例化上传类
         $upload->maxSize   =     3145728 ;// 设置附件上传大小
         //$upload->exts      =     array('jpg', 'gif', 'png', 'csv');// 设置附件上传类型
         $upload->rootPath  = C('UPLOAD_PATH');//"\\\\nanitfp01\\Public\\Departments\\Public\\2016 Campus Deck\\2017\\CV\\"; // 设置附件上传根目录
         $upload->savePath  =    ''; //I('dept').'_Test\\'; // 设置附件上传（子）目录
         $upload->autoSub = true;
-        $upload->subName = 'resume';
+        $upload->subName = $dept;
 
-        $upload->saveName = $name.date("Y-m-d");//I('name').'_'.I('dept').'_'.date("Y-m-d");//array('filename',I('name'),I('dept'));
+        $upload->saveName = $name.' '.date("Y-m-d");//I('name').'_'.I('dept').'_'.date("Y-m-d");//array('filename',I('name'),I('dept'));
         // 上传文件
         $this->response($upload->upload(),'json');
     }

0b5b27e79b887b0437fc824bf6d78344b752c1e4
Liang <lhao@merkleinc.com>
2017-09-29 13:24:22 +0800

    change config to prod

--- home.php
+++ home.php
@@ -15,12 +15,14 @@
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
+// define('APP_DEBUG',True);
+define('APP_DEBUG',FALSE);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
 define('BUILD_DIR_SECURE', false);
-define('APP_STATUS','home');
+// define('APP_STATUS','home');
+define('APP_STATUS','work');
 define('BIND_MODULE','Home');
 // define('BIND_CONTROLLER','Index');
 // 引入ThinkPHP入口文件
